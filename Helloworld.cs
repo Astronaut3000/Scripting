2eb3f6ce3da844a7530f909e83e3f0a7091b4fdc
Astronaut3000 <pavel.tornado.s@gmail.com>
2025-03-08 16:49:47 +0300

    Add files via upload

---
+++ Helloworld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //вывод информации в консоль
+        print("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
